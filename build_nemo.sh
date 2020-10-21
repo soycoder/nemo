9d6c9f090fbcee8f7902161be9443a13dd1e4cca
soycoder <43911669+soycoder@users.noreply.github.com>
2020-10-21 16:46:52 +0700

    Update build_nemo.sh

--- build_nemo.sh
+++ build_nemo.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
-source ~/nemo/apps/hpcx-v2.6.0-gcc-MLNX_OFED_LINUX-4.7-1.0.0.1-redhat7.7-x86_64/hpcx-init.sh \
-&& hpcx_load \
-&& export APPROOT=/tarafs/data/home/awongsor/nemo \
-&& cd $APPROOT/apps/nemo-4.0 \
-&& sed -i 's/\/usr\/bin\/mpif90/mpif90/g' arch/arch-linux_gfortran.fcm \ 
-&& sed -i 's/\/usr\/local\/netcdf/$APPROOT\/deps\/netcdf4/g' arch/arch-linux_gfortran.fcm \ 
-&& sed -i 's/\/usr\/local\/hdf5/$APPROOT\/deps\/hdf5\/hdf5/g' arch/arch-linux_gfortran.fcm \ 
-&& ./makenemo -m linux_gfortran -r GYRE_PISCES -n hpcx3 -j $(nproc) 
+source ~/nemo/apps/hpcx-v2.6.0-gcc-MLNX_OFED_LINUX-4.7-1.0.0.1-redhat7.7-x86_64/hpcx-init.sh
+hpcx_load
+export APPROOT=/tarafs/data/home/awongsor/nemo
+cd $APPROOT/apps/nemo-4.0
+sed -i 's/\/usr\/bin\/mpif90/mpif90/g' arch/arch-linux_gfortran.fcm
+sed -i 's/\/usr\/local\/netcdf/$APPROOT\/deps\/netcdf4/g' arch/arch-linux_gfortran.fcm
+sed -i 's/\/usr\/local\/hdf5/$APPROOT\/deps\/hdf5\/hdf5/g' arch/arch-linux_gfortran.fcm
+./makenemo -m linux_gfortran -r GYRE_PISCES -n hpcx3 -j $(nproc) 
 
  
